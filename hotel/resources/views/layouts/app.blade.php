07bb454cef92abc3d1f9eeee2aba69d24bf22c30
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-06-05 22:56:46 +0300

    made new login css rit c un v uz register form

--- hotel/resources/views/layouts/app.blade.php
+++ hotel/resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="public/style.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="hÍm/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
         <!-- Styles -->
 
         <!-- Scripts -->
--- hotel/resources/views/layouts/guest.blade.php
+++ hotel/resources/views/layouts/guest.blade.php
@@ -10,16 +10,18 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="public/style.css" rel="stylesheet">
-        
+        <link href="{{ asset('style.css') }}" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="precoÍgoogleapis.com/css2?familyÍto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <body >
+        <div>
+            <div>
                 {{ $slot }}
             </div>
         </div>
     </body>
 </html>
+ß

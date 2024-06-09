e314be7932fb82249cdffb705a2389b03bf847b5
GustavsLieknins <126756140+GustavsLieknins@users.noreply.github.com>
2024-06-09 23:50:44 +0300

    123

--- hotel/resources/views/index.blade.php
+++ hotel/resources/views/index.blade.php
@@ -1,3 +1,171 @@
 <x-app-layout>
-    <h1>Hey</h1>
+    <div class="wrapper">
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+
+        <a href="/" class="card-a">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/1717884320_c1McnkC.jpg" alt="">
+                </div>
+                <div class="card-info">
+                    <div class="card-title">
+                        <h1>Ваша уникальная страница</h1>
+                    </div>
+                    <div class="card-little-info">
+                        <span class="card-first-span">Riga raj</span>
+                        <span>|</span>
+                        <span>1432 left</span>
+                    </div>
+                    <div class="card-price">
+                        <p>€15</p>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
 </x-app-layout>
